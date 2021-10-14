8115ccacd7e70d1a4dee7b68c1e2c4ce8aa8c760
Jana Kantorová <janakantorova@Jana-MacBook-Pro.local>
2021-10-14 18:26:42 +0200

    add FocusedElement & Cookies test

---
+++ training/src/test/java/ThirdExerciseTests.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ThirdExerciseTests {
+   WebDriver driver; 
+
+   @Before
+   public void setUp()
+   {
+       driver = new ChromeDriver();
+   }
+
+   @After
+   public void tearDown()
+   {
+       driver.quit();
+   }
+
+   @Test
+   public void FocusedElement()
+   {
+    driver.get("https://example.cypress.io/commands/actions");
+    WebElement password = driver.findElement(By.cssSelector(".action-focus"));
+
+    Actions actions = new Actions(driver);
+    actions.moveToElement(password).click().perform();
+    assertTrue(driver.findElement(By.xpath("//*[@for='password1']")).getAttribute("style").contains("color: orange"));
+   }
+
+   @Test
+   public void Cookies()
+   {
+       driver.get("https://example.cypress.io/commands/cookies");
+       WebElement element = driver.findElement(By.cssSelector("#getCookie .set-a-cookie"));
+       element.click();
+
+       var cookie = driver.manage().getCookieNamed("token");
+       assertEquals("123ABC", ((Cookie) cookie).getValue());
+   }
+}
